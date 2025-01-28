123b83d8493316925c077f9e820272677b45220d
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-01-29 00:58:16 +0300

    İcons

--- Redux/src/components/ProductCard.jsx
+++ Redux/src/components/ProductCard.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BsThreeDots } from 'react-icons/bs';
 
 const ProductCard = ({ dt }) => {
     return (
         <div className="w-[200px] h-[200px] relative m-2 rounded-md">
             <img src={dt?.url} className="w-full h-full absolute rounded-md" alt=""></img>
+            <div className="absolute left-0 bottom-0 bg-indigo-600 text-white w-full">
+                <div className="text-lg font-semibold">{dt?.name}</div>
+                <div>{dt?.price}</div>
+            </div>
+            <div className="absolute right-2 top-0">
+                <BsThreeDots color='white' size={24} />
+            </div>
         </div>
     );
 };
 ProductCard.propTypes = {
     dt: PropTypes.shape({
-        url: PropTypes.string.isRequired
+        url: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired
     }).isRequired
 };
 
