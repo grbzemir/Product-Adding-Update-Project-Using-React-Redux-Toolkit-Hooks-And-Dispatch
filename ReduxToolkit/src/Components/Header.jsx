7c5ee290d7a6ff8d542ee506fb2bc3277b5682b3
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-29 17:51:59 +0300

    .

--- ReduxToolkit/src/Components/Header.jsx
+++ ReduxToolkit/src/Components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import PostAddIcon from '@mui/icons-material/PostAdd';
-
+import { useDispatch } from 'react-redux';
+import { modalFunction } from '../Redux/modalSlice';
 
 const Header = () => {
+
+    const dispatch = useDispatch();
+
     return (
         <div className="flex items-center justify-between bg-indigo-600 text-white px-4  py-3">
             <div className="text-3xl">Gif Upload Project</div>
@@ -14,7 +18,7 @@ const Header = () => {
                     </select>
                 </div>
                 <input className="h-10 rounded-lg px-4" type="text" placeholder='Arama Yapınız...' />
-                <div className="bg-indigo-800 w-10 h-10 rounded-full flex items-center justify-center cursor-pointer">
+                <div onClick={() => dispatch(modalFunction())} className="bg-indigo-800 w-10 h-10 rounded-full flex items-center justify-center cursor-pointer">
                     <PostAddIcon size={24} />
                 </div>
             </div>
--- ReduxToolkit/src/Pages/Product.jsx
+++ ReduxToolkit/src/Pages/Product.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import ProductCard from '../Components/ProductCard'
 
+
 const Product = () => {
+
+    const { modal } = useSelector((state) => state.modal)
+    console.log("Modal State:", modal);
     return (
         <div>
-
+            <ProductCard />
+            {modal && <modal />}
         </div>
     )
 }
 
-export default Product
+export default Product;
